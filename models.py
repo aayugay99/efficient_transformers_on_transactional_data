f9dedb49b3bc7f3ca8b8bc5fdd5c1fa85b01cf64
Egor Fadeev <74717504+linglingec@users.noreply.github.com>
2023-05-23 18:54:39 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn 
-
+from linear_attention_transformer import LinearAttentionTransformerLM, LinearAttentionTransformer
 import math
 
 class TransactionEncoder(nn.Module):
@@ -119,8 +119,62 @@ class TransformerModel(nn.Module):
 
 
 class LinearTransformerModel(nn.Module):
-    # TODO: implement model
-    pass
+    def __init__(
+            self, 
+            feature_embeddings: dict[str, tuple[int, int]], 
+            linear_proj: int=None,
+            n_head: int=8, 
+            dim_feedforward: int=128, 
+            dropout: float=0.1, 
+            num_layers: int=6, 
+            head_hidden: int=128,
+            max_len: int=1000,
+        ):
+        super().__init__()
+
+        self.transaction_encoder = TransactionEncoder(feature_embeddings, linear_proj=linear_proj)
+        self.embedding_dim = self.transaction_encoder.embedding_dim
+        self.cat_cols = list(feature_embeddings.keys())
+        self.num_classes_dict = {key: num_classes for key, (num_classes, _) in feature_embeddings.items()}
+        
+        self.pos_emb = PositionalEncoding(self.embedding_dim, dropout, max_len)
+
+        self.transformer_encoder = LinearAttentionTransformer(
+            dim = dim_feedforward, 
+            depth = num_layers,
+            heads = n_head, 
+            bucket_size = bucket_size, 
+            ff_dropout = dropout,
+            causal = True
+        )
+        
+        self.heads = nn.ModuleDict({
+            key: Head(
+                self.embedding_dim, 
+                head_hidden, 
+                num_classes
+            ) for key, num_classes in self.num_classes_dict.items()
+        })
+
+    def forward(self, x: torch.Tensor, device: str="cpu") -> torch.Tensor:
+        N, S = x[self.cat_cols[0]].shape
+        embeddings = self.transaction_encoder(x, device=device)
+        embeddings = self.pos_emb(embeddings)
+        
+        attn_mask = self.generate_square_subsequent_mask(S).to(device)
+        padding_mask = self.generate_padding_mask(x[self.cat_cols[0]]).to(device)
+        embeddings = self.transformer_encoder(embeddings, mask=attn_mask, is_causal=True, src_key_padding_mask=padding_mask)
+
+        logits = {key: self.heads[key](embeddings) for key in self.cat_cols}
+        return logits
+
+    @staticmethod
+    def generate_square_subsequent_mask(sz: int) -> torch.Tensor:
+        return torch.triu(torch.full((sz, sz), True), diagonal=1).bool()
+    
+    @staticmethod
+    def generate_padding_mask(x: torch.Tensor) -> torch.Tensor:
+        return torch.where(x == 0, True, False).bool()
 
 
 class PerformerModel(nn.Module):
