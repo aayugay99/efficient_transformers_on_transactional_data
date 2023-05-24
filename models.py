1001121a26cfede690187ea7c7c70f2f0c2d5d98
Aleksandr Yugay <sasha.yugay1999@mail.ru>
2023-05-24 19:56:17 +0300

    another implementation of performer

--- models.py
+++ models.py
@@ -2,8 +2,10 @@ import torch
 import torch.nn as nn 
 
 import math
+import copy
 
-from performer_pytorch import Performer
+from performer_pytorch import Performer, SelfAttention
+
 
 class TransactionEncoder(nn.Module):
     def __init__(self, feature_embeddings, linear_proj: int=None):
@@ -125,6 +127,55 @@ class LinearTransformerModel(nn.Module):
     pass
 
 
+class Block(nn.Module):
+    def __init__(
+            self, 
+            d_model, 
+            dim_feedforward,
+            dropout,
+            self_attention
+        ):
+        super().__init__()
+
+        self.self_attn = self_attention
+        
+        self.linear1 = nn.Linear(d_model, dim_feedforward)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_feedforward, d_model)
+
+        self.norm1 = nn.LayerNorm(d_model, eps=1e-5)
+        self.norm2 = nn.LayerNorm(d_model, eps=1e-5)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.activation = nn.GELU()
+
+    def forward(self, x, input_mask=None):
+        x = self.norm1(x + self._sa_block(x, input_mask=input_mask))
+        x = self.norm2(x + self._ff_block(x))
+        return x
+
+    def _sa_block(self, x, input_mask=None):
+        x = self.self_attn(x, input_mask=input_mask)
+        return self.dropout1(x)
+    
+    def _ff_block(self, x):
+        x = self.linear2(self.dropout(self.activation(self.linear1(x))))
+        return self.dropout2(x)
+    
+
+class Encoder(nn.Module):
+    def __init__(self, block, num_layers):
+        super().__init__()
+
+        self.blocks = nn.ModuleList([copy.deepcopy(block) for i in range(num_layers)])
+
+    def forward(self, x, input_mask=None):
+        for block in self.blocks:
+            x = block(x, input_mask)
+        return x
+
+
 class PerformerModel(nn.Module):
     def __init__(
             self, 
@@ -133,26 +184,12 @@ class PerformerModel(nn.Module):
             n_head: int=8, 
             dropout: float=0.1, 
             num_layers: int=6, 
+            dim_feedforward: int=128,
             head_hidden: int=128,
             max_len: int=1000,
-            dim_feedforward: int=128,
             dim_head: int=32,
-            local_attn_heads: int=0,
+            local_heads: int=0,
             local_window_size: int=256,
-            ff_mult: int=4,
-            feature_redraw_interval: int=1000,
-            reversible: bool=False,
-            ff_chunks: int=1,
-            generalized_attention: bool=False,
-            use_scalenorm: bool=False,
-            use_rezero: bool=False,
-            ff_glu: bool=False,
-            cross_attend: bool=False,
-            no_projection: bool=False,
-            auto_check_redraw: bool=True,
-            qkv_bias: bool=True,
-            attn_out_bias: bool=True,
-            shift_tokens: bool=False
         ):
         super().__init__()
 
@@ -163,30 +200,21 @@ class PerformerModel(nn.Module):
         
         self.pos_emb = PositionalEncoding(self.embedding_dim, dropout, max_len)
 
-        self.transformer_encoder = Performer(
-            dim = self.embedding_dim, 
-            depth = num_layers,
-            heads = n_head, 
-            ff_dropout = dropout,
-            causal = True,
-            dim_head = dim_head,
-            local_attn_heads = local_attn_heads,
-            local_window_size = local_window_size,
-            ff_mult = ff_mult,
-            feature_redraw_interval = feature_redraw_interval,
-            reversible = reversible,
-            ff_chunks = ff_chunks,
-            generalized_attention = generalized_attention,
-            use_scalenorm = use_scalenorm,
-            use_rezero = use_rezero,
-            ff_glu = ff_glu,
-            cross_attend = cross_attend,
-            no_projection = no_projection,
-            auto_check_redraw = auto_check_redraw,
-            qkv_bias = qkv_bias,
-            attn_out_bias = attn_out_bias,
-            shift_tokens = shift_tokens
+        sa_module = SelfAttention(
+            self.embedding_dim, 
+            causal=True, 
+            heads=n_head,
+            dim_head=dim_head,
+            local_heads=local_heads,
+            local_window_size=local_window_size
         )
+        self.encoder_layer = Block(
+            self.embedding_dim, 
+            dim_feedforward, 
+            dropout, 
+            sa_module
+        )
+        self.transformer_encoder = Encoder(self.encoder_layer, num_layers)
         
         self.heads = nn.ModuleDict({
             key: Head(
@@ -197,20 +225,14 @@ class PerformerModel(nn.Module):
         })
 
     def forward(self, x: torch.Tensor, device: str="cpu") -> torch.Tensor:
-        N, S = x[self.cat_cols[0]].shape
         embeddings = self.transaction_encoder(x, device=device)
         embeddings = self.pos_emb(embeddings)
         
-        attn_mask = self.generate_square_subsequent_mask(S).to(device)
         padding_mask = self.generate_padding_mask(x[self.cat_cols[0]]).to(device)
         embeddings = self.transformer_encoder(embeddings, input_mask=padding_mask)
 
         logits = {key: self.heads[key](embeddings) for key in self.cat_cols}
         return logits
-
-    @staticmethod
-    def generate_square_subsequent_mask(sz: int) -> torch.Tensor:
-        return torch.triu(torch.full((sz, sz), True), diagonal=1).bool()
     
     @staticmethod
     def generate_padding_mask(x: torch.Tensor) -> torch.Tensor:
