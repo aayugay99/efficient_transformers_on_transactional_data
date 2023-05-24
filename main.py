f2e6ab5e9bd14868ab3997ebd60919dd45fe152c
Egor Fadeev <74717504+linglingec@users.noreply.github.com>
2023-05-24 21:47:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,120 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import KBinsDiscretizer
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+import time
+import wandb
+from yaml import load, CLoader as Loader
+
+from dataset import TransactionDataset, transaction_collate_fn
+from utils import train_model
+from models import TransformerModel, PerformerModel, LinearTransformerModel
+
+import argparse
+
+
+def main(path_to_config):
+    with open(path_to_config, "r") as f:
+        config = load(f, Loader)
+
+    wandb.login(key='a86c8d682f0e7113c3c719b513381330154220ec')
+
+    wandb.init(
+        entity="aayugay99",
+        project="deep-learning-project",
+        name=config["experiment_name"],
+        config=config
+    )
+
+    if config["dataset"] == "rosbank":
+        df = pd.read_csv('data/rosbank/train.csv')
+        df['TRDATETIME'] = pd.to_datetime(df['TRDATETIME'], format='%d%b%y:%H:%M:%S')
+        df = df.rename(columns={'cl_id':'client_id', 'MCC':'small_group', 'amount':'amount_rur'})
+
+    if config["dataset"] == "sberbank":
+        df = pd.read_csv('data/sberbank/transactions_train.csv')
+        df = df.rename({'trans_date': 'TRDATETIME'}, axis=1)
+
+
+    mcc_to_id = {mcc: i+1 for i, mcc in enumerate(df['small_group'].unique())}
+
+    df['amount_rur_bin'] = 1 + KBinsDiscretizer(10, encode='ordinal').fit_transform(df[['amount_rur']]).astype('int')
+    df['small_group'] = df['small_group'].map(mcc_to_id)
+
+    clients_train, clients_val_test = train_test_split(df["client_id"].unique(), test_size=0.2, random_state=42)
+    clients_val, clients_test = train_test_split(clients_val_test, test_size=0.5, random_state=42)
+
+    train_ds = TransactionDataset(
+        df[lambda x: x["client_id"].isin(clients_train)], 
+        id_col="client_id", 
+        dt_col="TRDATETIME", 
+        cat_cols=["small_group", "amount_rur_bin"],
+        min_length=config["min_length"],
+        max_length=config["max_length"],
+        random_slice=True
+    )
+
+    val_ds = TransactionDataset(
+        df[lambda x: x["client_id"].isin(clients_val)], 
+        id_col="client_id", 
+        dt_col="TRDATETIME", 
+        cat_cols=["small_group", "amount_rur_bin"],
+        min_length=config["min_length"],
+        max_length=config["max_length"],
+        random_slice=False
+    )
+
+    test_ds = TransactionDataset(
+        df[lambda x: x["client_id"].isin(clients_test)], 
+        id_col="client_id", 
+        dt_col="TRDATETIME", 
+        cat_cols=["small_group", "amount_rur_bin"],
+        min_length=config["min_length"],
+        max_length=config["max_length"],
+        random_slice=False
+    )
+
+    train_loader = DataLoader(train_ds, batch_size=config["batch_size"], shuffle=True, collate_fn=transaction_collate_fn)
+    val_loader = DataLoader(val_ds, batch_size=config["batch_size"], shuffle=False, collate_fn=transaction_collate_fn)
+    test_loader = DataLoader(test_ds, batch_size=config["batch_size"], shuffle=False, collate_fn=transaction_collate_fn)
+
+    # TODO: add support for different transformers
+    assert config["type"] in ["transformer", "performer", "reformer", "linear_transformer"]
+
+    if config["type"] == "transformer":
+        model = TransformerModel(**config["transformer_params"], max_len=config["max_length"])
+    elif config["type"] == "performer":
+        model = PerformerModel(max_len=config["max_length"], **{**config['transformer_params'], **config['performer_params']})
+    elif config["type"] == "reformer":
+        pass
+    elif config["type"] == "linear_transformer":
+        model = LinearTransformerModel(**config["linear_transformer_params"], max_len=config["max_length"])
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"])
+
+    train_model(
+        model, 
+        optimizer, 
+        {"train": train_loader, "val": val_loader, "test": test_loader}, 
+        n_epochs=config["n_epochs"],
+        warmup=config["warmup"],
+        device=config["device"],
+        save_path=config["save_path"]
+    )
+
+    wandb.finish()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Runs experiments with transformers on transactional data',
+    )
+
+    parser.add_argument('filename')
+    args = parser.parse_args()
+    
+    main(args.filename)
