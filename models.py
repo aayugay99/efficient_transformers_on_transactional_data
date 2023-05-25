2312298fc262d9cfde44ae96dbcc38b3591b4b85
Egor Fadeev <74717504+linglingec@users.noreply.github.com>
2023-05-25 18:00:56 +0300

    Update models.py

--- models.py
+++ models.py
@@ -140,9 +140,9 @@ class LinearTransformerModel(nn.Module):
             dim_feedforward: int=128,
             num_layers: int=6, 
             head_hidden: int=128,
+            dropout: float=0.0,
             max_len: int=100,
             dim_head: int=32,
-            dropout: float=0.0,
             local_attn_window_size: int=100,
             blindspot_size: int=1,
             n_local_attn_heads: int=5
